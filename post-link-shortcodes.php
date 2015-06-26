2afff8ff6c55648343b69cee65121edb78c5dad1
Evan Mattson <me@aaemnnost.tv>
2015-06-25 23:38:15 -0400

    update version and plugin uri

--- post-link-shortcodes.php
+++ post-link-shortcodes.php
@@ -2,10 +2,10 @@
 /*
 	Plugin Name: Post Link Shortcodes
 	Description: A plugin for dynamically adding a collection of shortcodes for building links to a given post/archive (of any type) or simply returning the requested URL!
-	Version: 0.3.1
+	Version: 0.4.0
 	Author: Evan Mattson
 	Author URI: http://aaemnnost.tv
-	Plugin URI: https://github.com/aaemnnosttv/Post-Link-Shortcodes
+	Plugin URI: https://github.com/aaemnnosttv/post-link-shortcodes
 	License: GPL2
 */
 
