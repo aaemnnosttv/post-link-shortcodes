10c77a716c513c22af32e5d39006ef3b2d4c9431
Evan Mattson <me@aaemnnost.tv>
2018-10-21 23:24:07 +0300

    configure wp-phpunit

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,24 +1,12 @@
 <?php
-/**
- * PHPUnit bootstrap file
- *
- * @package post-link-shortcodes
- */
-
-if (! $_tests_dir = getenv('WP_TESTS_DIR')) {
-    $_tests_dir = '/tmp/wordpress-tests-lib';
-}
-
-require_once __DIR__ . '/trait-shortcode-assertions.php';
+// Composer autoloader must be loaded before WP_PHPUNIT__DIR will be available
+require_once dirname(__DIR__) . '/vendor/autoload.php';
 
 // Give access to tests_add_filter() function.
-require_once $_tests_dir . '/includes/functions.php';
+require_once getenv('WP_PHPUNIT__DIR') . '/includes/functions.php';
 
-/**
- * Manually load the plugin being tested.
- */
-tests_add_filter('muplugins_loaded', function() {
-	require realpath(__DIR__ . '/../post-link-shortcodes.php');
+tests_add_filter('muplugins_loaded', function () {
+    require realpath(__DIR__ . '/../post-link-shortcodes.php');
 });
 
 /**
@@ -29,14 +17,14 @@ tests_add_filter('init', function () {
     register_post_type('secret', ['show_ui' => false]);
 
     $args = [
-        'public' => true,
-        'show_ui' => true,
+        'public'      => true,
+        'show_ui'     => true,
         'has_archive' => true,
-        'supports' => ['title','editor','thumbnail']
+        'supports'    => ['title', 'editor', 'thumbnail']
     ];
     register_post_type('cpt-with-dashes', $args);
     register_post_type('cpt_with_underscores', $args);
 });
 
 // Start up the WP testing environment.
-require $_tests_dir . '/includes/bootstrap.php';
+require getenv('WP_PHPUNIT__DIR') . '/includes/bootstrap.php';
